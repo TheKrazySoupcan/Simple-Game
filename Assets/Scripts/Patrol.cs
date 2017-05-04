dc06636a68ec36800ddc4f428d81c01631322412
TheKrazySoupcan <thekrazysoupcan@gmail.com>
2017-05-04 15:34:19 +1000

    Simple

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float movespeed;
+
+    private int currentPoint;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = patrolPoints[2].position;
+        currentPoint = 0;
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (transform.position == patrolPoints[currentPoint].position)
+        {
+            currentPoint++;
+
+        }
+
+        if (currentPoint >= patrolPoints.Length)
+        {
+            currentPoint = 0;
+
+        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, movespeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private float maxSpeed = 5f;
+
+    private Vector3 input;
+
+    private Rigidbody rigid;
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (rigid.velocity.magnitude < maxSpeed)
+        {
+            rigid.AddForce(input * moveSpeed);
+        }
+
+       
+
+    }
+}
